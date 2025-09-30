6c0fe30359890483e8628f4010f0fe82aec9cd6d
zhuzhonghua <zhuzhonghua>
2025-09-30 09:49:36 +0800

    栈，单调栈

--- SwordOffer/6.栈/LCR 040. 最大矩形.cpp
+++ SwordOffer/6.栈/LCR 040. 最大矩形.cpp
@@ -56,39 +56,39 @@ matrix[i][j] 为 '0' 或 '1'
 
 int largestRectangleArea(vector<int> &heights)
 {
-  std::stack<int> stack;
-  stack.push(-1);
-  int res = 0;
+    std::stack<int> stack;
+    stack.push(-1);
+    int res = 0;
 
-  // 遍历数组
-  for (int i = 0; i < heights.size(); i++)
-  {
-    // 遍历元素与栈顶元素判断，遇到栈顶元素比遍历元素大的要进行出栈
-    while (stack.top() != -1 && heights[stack.top()] >= heights[i])
+    // 遍历数组
+    for (int i = 0; i < heights.size(); i++)
     {
-      int topI = stack.top();
-      stack.pop();
+        // 遍历元素与栈顶元素判断，遇到栈顶元素比遍历元素大的要进行出栈
+        while (stack.top() != -1 && heights[stack.top()] >= heights[i])
+        {
+            int topI = stack.top();
+            stack.pop();
 
-      // 计算面积
-      int area = heights[topI] * (i - stack.top() - 1);
-      res = std::max(res, area);
+            // 计算面积
+            int area = heights[topI] * (i - stack.top() - 1);
+            res = std::max(res, area);
+        }
+        // 并且最终遍历元素要入栈
+        stack.push(i);
     }
-    // 并且最终遍历元素要入栈
-    stack.push(i);
-  }
 
-  // 遍历完后，栈中还有元素，继续
-  while (stack.top() != -1)
-  {
-    int topI = stack.top();
-    stack.pop();
+    // 遍历完后，栈中还有元素，继续
+    while (stack.top() != -1)
+    {
+        int topI = stack.top();
+        stack.pop();
 
-    // 计算面积
-    int area = heights[topI] * (heights.size() - stack.top() - 1);
-    res = std::max(res, area);
-  }
+        // 计算面积
+        int area = heights[topI] * (heights.size() - stack.top() - 1);
+        res = std::max(res, area);
+    }
 
-  return res;
+    return res;
 }
 
 /**
@@ -100,65 +100,65 @@ int largestRectangleArea(vector<int> &heights)
  */
 int maximalRectangle(vector<string> &matrix)
 {
-  if (matrix.empty())
-  {
-    return 0;
-  }
-  std::vector<int> heights(matrix[0].length(), 0);
-  int res = 0;
-  for (std::string str : matrix)
-  {
-    // 求直方图的高度
-    for (int i = 0; i < str.length(); i++)
+    if (matrix.empty())
     {
-      if (str[i] == '0')
-      {
-        heights[i] = 0;
-      }
-      else
-      {
-        heights[i]++;
-      }
+        return 0;
     }
-    res = std::max(res, largestRectangleArea(heights));
-  }
+    std::vector<int> heights(matrix[0].length(), 0);
+    int res = 0;
+    for (std::string str : matrix)
+    {
+        // 求直方图的高度
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str[i] == '0')
+            {
+                heights[i] = 0;
+            }
+            else
+            {
+                heights[i]++;
+            }
+        }
+        res = std::max(res, largestRectangleArea(heights));
+    }
 
-  return res;
+    return res;
 }
 
 int main()
 {
-  std::cout << "Hello, Algorithm!" << std::endl;
+    std::cout << "Hello, Algorithm!" << std::endl;
 
-  vector<string> matrix = {"10100", "10111", "11111", "10010"};
+    vector<string> matrix = {"10100", "10111", "11111", "10010"};
 
-  for (auto ele : matrix)
-  {
-    std::cout << ele << ",";
-  }
-  std::cout << std::endl;
+    for (auto ele : matrix)
+    {
+        std::cout << ele << ",";
+    }
+    std::cout << std::endl;
 
-  auto res = maximalRectangle(matrix);
+    auto res = maximalRectangle(matrix);
 
-  std::cout << "res:" << res << std::endl;
+    std::cout << "res:" << res << std::endl;
 
-  // 遍历1维数组
-  // for (auto ele : res)
-  // {
-  //   std::cout << ele << ",";
-  // }
-  // std::cout << std::endl;
+    // 遍历1维数组
+    // for (auto ele : res)
+    // {
+    //   std::cout << ele << ",";
+    // }
+    // std::cout << std::endl;
 
-  // 遍历2维数组
-  // for (vector<int> ele : res)
-  // {
-  //     for (auto element : ele)
-  //     {
-  //         std::cout << element << ",";
-  //     }
-  //     std::cout << std::endl;
-  // }
-  // std::cout << std::endl;
+    // 遍历2维数组
+    // for (vector<int> ele : res)
+    // {
+    //     for (auto element : ele)
+    //     {
+    //         std::cout << element << ",";
+    //     }
+    //     std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
 
-  return 0;
+    return 0;
 }
